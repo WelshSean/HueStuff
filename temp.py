7abad0baad524785ccb8754e4f737890aa8bd472
Sean Clarke <sean@garageserver.localdomain>
2021-05-02 21:35:20 +0100

    Added method to map UIDs to human friendly names - hack AF and probably should refactor the whole DS to use UIDs as dict keys but it works! for now.......

--- temp.py
+++ temp.py
@@ -31,17 +31,29 @@ def findSensorByType(sensortype):
 
 def getTemps():
     temps=[]
+    # Get mappings of UUID to Human Friendly name
+    mappings = getID2NameMapping()
+    print(mappings)
     sensors = findSensorByType("ZLLTemperature")
     if sensors is not None:
         for sensor in sensors:
             tempdict={}
-            tempdict["id"]=sensor[0]
-            tempdict["name"]=sensor[1]["name"]
+            tempdict["id"]=sensor[1]["uniqueid"]
+            tempdict["name"]=mappings[tempdict["id"][:-5]]
             tempdict["temperature"]=sensor[1]["state"]["temperature"]/100
             temps.append(tempdict)
     else:
         print("list empty")
     return temps
 
+def getID2NameMapping():
+    mappings={}
+    sensors = findSensorByType("ZLLPresence")
+    for sensor in sensors:
+        print(sensor[1]["uniqueid"][:-5])
+        mappings[sensor[1]["uniqueid"][:-5]] = sensor[1]["name"]
+    return mappings
+
 if __name__ == "__main__":
+    #print(getID2NameMapping())
     print(json.dumps(getTemps()))
