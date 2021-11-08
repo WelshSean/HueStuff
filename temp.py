ce2c46978286f73ba9a6426092e48c62570765ce
Sean Clarke <sean@garageserver.localdomain>
2021-11-08 22:06:19 +0000

    Refactored to be able to cope with multiple hubs

--- temp.py
+++ temp.py
@@ -3,56 +3,60 @@
 import json
 import requests
 import os
-HUE_BRIDGE=os.environ["HUE_BRIDGE"]
-USER_ID=os.environ["USER_ID"]
+#HUE_BRIDGE=os.environ["HUE_BRIDGE"]
+#USER_ID=os.environ["USER_ID"]
+HUE_DETAILS=os.environ["HUE_DETAILS"]
 
 
+#SENSOR_URL = 'http://' + HUE_BRIDGE + '/api/' + USER_ID + '/sensors'
 
-SENSOR_URL = 'http://' + HUE_BRIDGE + '/api/' + USER_ID + '/sensors'
-
-def getSensorInfo():
+def getSensorInfo(sensor_url):
   try:
-    response = requests.get(SENSOR_URL)
+    response = requests.get(sensor_url)
     json_data = json.loads(response.text)
     return json_data
   except:
     logger.exception("exception occurred")
 
-def printSensorInfo():
-    print( json.dumps(getSensorInfo(), indent=4, sort_keys=True))
+def printSensorInfo(sensor_url):
+    print( json.dumps(getSensorInfo(sensor_url), indent=4, sort_keys=True))
 
 
-def findSensorByType(sensortype):
+def findSensorByType(sensortype,sensor_url):
     ids=[]
-    for (key, data) in getSensorInfo().items():
+    for (key, data) in getSensorInfo(sensor_url).items():
         if data["type"] == sensortype:
             ids.append((key,data))
     return ids
 
 def getTemps():
     temps=[]
-    # Get mappings of UUID to Human Friendly name
-    mappings = getID2NameMapping()
-    #print(mappings)
-    sensors = findSensorByType("ZLLTemperature")
-    if sensors is not None:
-        for sensor in sensors:
-            if sensor[1]["state"]["temperature"] is None:
-                 #print("Found None value - skipping")
-                 continue
-            #print(sensor)
-            tempdict={}
-            temp_id=sensor[1]["uniqueid"]
-            tempdict["name"]=mappings[temp_id[:-5]]
-            tempdict["temperature"]=sensor[1]["state"]["temperature"]/100
-            temps.append(tempdict)
-    else:
-        print("list empty")
+    for detail in HUE_DETAILS.split():
+        print("DETAIL: "+detail)
+        (HUE_BRIDGE,USER_ID)=detail.split(':') 
+        sensor_url = 'http://' + HUE_BRIDGE + '/api/' + USER_ID + '/sensors'
+        # Get mappings of UUID to Human Friendly name
+        mappings = getID2NameMapping(sensor_url)
+        #print(mappings)
+        sensors = findSensorByType("ZLLTemperature", sensor_url)
+        if sensors is not None:
+            for sensor in sensors:
+                if sensor[1]["state"]["temperature"] is None:
+                    #print("Found None value - skipping")
+                    continue
+                #print(sensor)
+                tempdict={}
+                temp_id=sensor[1]["uniqueid"]
+                tempdict["name"]=mappings[temp_id[:-5]]
+                tempdict["temperature"]=sensor[1]["state"]["temperature"]/100
+                temps.append(tempdict)
+        else:
+            print("list empty")
     return temps
 
-def getID2NameMapping():
+def getID2NameMapping(sensor_url):
     mappings={}
-    sensors = findSensorByType("ZLLPresence")
+    sensors = findSensorByType("ZLLPresence", sensor_url)
     for sensor in sensors:
         mappings[sensor[1]["uniqueid"][:-5]] = sensor[1]["name"]
     return mappings
